b8fa64304f4d086cfcb2dfa6c74d3d58df51d014
Sheethal31 <75903042+Ella311@users.noreply.github.com>
2022-12-17 22:04:08 +0530

    added categories file

---
+++ api/routes/categories.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+
+//ADD NEW CATEGORY
+router.post("/", async (req, res) => {
+  const newCategory = new Category(req.body);
+  try {
+    const savedCategory = await newCategory.save();
+    res.status(200).json(savedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL CATEGORY
+router.get("/", async (req, res) => {
+    try {
+      const categories = await Category.find();
+      res.status(200).json(categories);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
