9208950dd9e30ab1dfdfef916c4feee695652bf8
Samvrutha Shaji <54467949+SamvruthaShaji@users.noreply.github.com>
2022-12-17 23:09:26 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import Header from "../../header/Header"
+import Posts from "../../post/Post"
+import Sidebar from "../../sidebar/Sidebar"
 import "./home.css"
 
 export default function Home() {
   return (
-    <div className="home">
+      <>
       <Header/>
+      <div className="home">
+        <Posts/>
+        <Sidebar/>
+      </div>
       homepage
-    </div>
+      </>
+   
+    
   )
 }
