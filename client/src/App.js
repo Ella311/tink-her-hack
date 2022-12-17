7d3f2a7b444fb5c832bd15b66617f98510a00b5b
Samvrutha <samvrutha.shaji01@gmail.com>
2022-12-18 00:08:44 +0530

    updated login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import Home from "./pages/home/Home";
-
-
+import Home from "./pages/home/Home"
+import TopBar from "./TopBar/TopBar"
+import Login from "./pages/login/Login"
 function App() {
   return(
+ <>
+ <TopBar/>
  <Home/>
-
+ <Login/>
+ </>
   );
 }
 export default App;
