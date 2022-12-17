714d8e3c71bf9c7e36b0bf6c270a05a7f8da1f84
Dencymol Baby <75939427+Dency675@users.noreply.github.com>
2022-12-18 01:35:11 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/home/Home"
 import TopBar from "./TopBar/TopBar"
 import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 function App() {
   return(
  <>
  <TopBar/>
  <Home/>
  <Login/>
+  <Register/>
  </>
   );
 }
