dea14b0721e4e1504461ef5d118ce8986abdc936
Samvrutha Shaji <54467949+SamvruthaShaji@users.noreply.github.com>
2022-12-18 13:13:09 +0530

    Update TopBar.jsx

--- client/src/TopBar/TopBar.jsx
+++ client/src/TopBar/TopBar.jsx
@@ -1,4 +1,6 @@
 import "./topbar.css"
+import {Link} from "react-router-dom"
+
 export default function TopBar(){
     return (
         <div className="top">
@@ -11,15 +13,30 @@ export default function TopBar(){
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">
+                    <Link className="home" to="/" style={{textDecoration: "none", color: "inherit"}}>
+                        HOME
+                    </Link>
+                    </li>
                     <li className="topListItem">ABOUT</li>
                     <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                    <Link className="link" to="/write" style={{textDecoration: "none", color: "inherit"}}>
+                        WRITE
+                    </Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/login" style={{textDecoration: "none", color: "inherit"}}>
+                        LOGIN
+                    </Link>
+                    </li>
+                  
                 </ul>
             </div>
             <div className="topRight">
-                <img className="topImage" src="https://cdn.pixabay.com/photo/2020/05/09/13/29/photographer-5149664_960_720.jpg" alt="" />
+                <a href="/user">
+                    <img className="topImage"src="https://cdn.pixabay.com/photo/2020/05/09/13/29/photographer-5149664_960_720.jpg" alt="" />
+                </a>
                 <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
                 <i class="topSearchIcon fa-solid fa-bars"></i>
             </div>
