0e7ef2587ae0096d287e1857ee934d38b97bebcf
Samvrutha Shaji <54467949+SamvruthaShaji@users.noreply.github.com>
2022-12-18 12:37:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import Home from "./pages/home/Home"
 import TopBar from "./TopBar/TopBar"
-import Login from "./pages/login/Login"
-import Register from "./pages/register/Register"
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Write from "./pages/write/Write"
+import User from "./pages/user/User"
+
+import { BrowserRouter as Router,Switch,Route,Routes } from "react-router-dom";
+
 function App() {
   return(
  <>
+ <Router>
  <TopBar/>
- <Home/>
- <Login/>
-  <Register/>
- </>
+ <Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/login" element={<Login/>}/>
+<Route path="/register" element={<Register/>}/>
+<Route path="/write" element={<Write/>}/>
+<Route path ='/user' element={<User/>}/>
+ </Routes>
+ </Router>
+ 
+</>
   );
 }
 export default App;
