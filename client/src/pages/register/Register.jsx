2028a66d9932f56bda452dfa67876366677737df
Samvrutha Shaji <54467949+SamvruthaShaji@users.noreply.github.com>
2022-12-18 13:22:57 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,9 +1,9 @@
 import "./register.css"
-
-export default function Login() {
+import {Link} from "react-router-dom"
+export default function Register() {
   return (
-    <div className="login_box">
-        <div className="left">
+    <div className="register_box">
+        <div className="register_left">
             <div className="top_link">
                 <a href="">
                     <img src="https://drive.google.com/u/0/uc?id=16U__U5dJdaTfNGobB_OpwAJ73vM50rPV&export=download" alt="" />Return home
@@ -12,19 +12,24 @@ export default function Login() {
             <div className="contact">
                 <form action="">
                     <h3>Register</h3>
-                    <div className="login__field">
+                    <div className="register__field">
                         <i class="button__icon fa-solid fa-user"></i>
-                        <input type="text" className="login__input" placeholder="Username" />
+                        <input type="text" className="register__input" placeholder="Username" />
                     </div>
-                    <div className="login__field">
+                    <div className="register__field">
                     <i class=" button__icon fa-solid fa-envelope"></i>
-                        <input type="text" className="login__input" placeholder="E-mail" />
+                        <input type="text" className="register__input" placeholder="E-mail" />
                     </div>
-                    <div className="login__field">
+                    <div className="register__field">
                     <i class=" button__icon fa-solid fa-lock"></i>
-                        <input type="password" className="login__input" placeholder="Password"/>
+                        <input type="password" className="register__input" placeholder="Password"/>
                     </div>
-                    <button className="submit">SUBMIT</button>
+                    <div>
+                    <Link className="links" to="/login" style={{textDecoration: "none", color: "inherit"}}>
+                        Dont have account?
+                    </Link>
+                    </div>
+                    <button className="register_submit">SUBMIT</button>
                 </form>
             </div>
         </div>
