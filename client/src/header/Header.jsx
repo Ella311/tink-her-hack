71117ca63848cf9375a0f040fcad5671866fd539
Samvrutha Shaji <54467949+SamvruthaShaji@users.noreply.github.com>
2022-12-18 13:25:59 +0530

    Update Header.jsx

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,17 +1,15 @@
 import "./header.css";
+import React from 'react';
 
 export default function Header() {
   return (
     <div className="header">
      <div className="headerTitles">
-        <span className="headerTitleSm">redefines your boundaries</span>
         <span className="headerTitleLg">BlogSpace</span>
+        <span className="headerTitleSm">redefines your boundaries</span>
+
       </div>
-      <img
-        className="headerImg"
-        src="https://wallpaperaccess.com/full/2274267.jpg"
-        alt=""
-      />
+      <div className="headerImg"></div>
     </div>
   );
 }
